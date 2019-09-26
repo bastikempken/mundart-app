5b72117221dab42812789ac687ae6116a8e2cf06
Sebastian Kempken <sebastiankempken@mailbox.org>
2019-09-26 21:02:33 +0200

    Client package.json

---
+++ deploy-azure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+readonly ZIP_FILE=deploy-content.zip
+# clear zip
+rm $ZIP_FILE
+
+# Build source
+npm install
+npm run build-client
+
+# Zip
+zip -r $ZIP_FILE node_modules/ index.js client/build/ > /dev/null
+
+# Extract credentials
+rawUserInput=$(cat deploy-credentials.json | jq '.id | tostring')
+rawPasswordInput=$(cat deploy-credentials.json | jq '.password | tostring')
+user=${rawUserInput:1:((${#rawUserInput}-2))}
+password=${rawPasswordInput:1:((${#rawPasswordInput}-2))}
+echo "USER: $user and PASSWORD: $password"
+
+# Deploy to Azure
+curl -X POST -u $user:$password https://mundart-test-app.scm.azurewebsites.net/api/zipdeploy -T $ZIP_FILE
+
+rm $ZIP_FILE
