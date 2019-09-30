42d188b7382201eaba4a55cba57454d431a0fbec
Sebastian Kempken <sebastiankempken@mailbox.org>
2019-09-30 23:36:19 +0200

    Error Page v2 and todos

---
+++ client/src/SpinnerComponent.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Spinner } from "react-bootstrap";
+import Loader from "react-loader-advanced";
+import {LOADING_OFF} from "./reducers/action-types";
+import {navigateToError} from "./AppRouter";
+
+const delay = 3000;
+
+class SpinnerComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleCallback = () => {
+      if(this.props.show){
+          setTimeout(() => {
+                if(this.props.show){
+                    navigateToError();
+                    this.props.loadingOff();
+                }
+          }, 4*1000);
+      }
+    };
+
+    componentDidMount() {
+        this.handleCallback();
+    }
+
+    componentDidUpdate() {
+        this.handleCallback();
+    }
+
+    render() {
+        return (
+            <>
+                <Loader
+                    show={this.props.show}
+                    message={<Spinner animation="grow" variant="primary" />}
+                    backgroundStyle={{
+                        backgroundColor: "white",
+                        position: "fixed",
+                        zIndex: 400
+                    }}
+                    hideContentOnLoad={true}
+                    transitionConfig={{
+                        transitionName: "",
+                        transitionEnterTimeout: 1,
+                        transitionLeaveTimeout: delay
+                    }}
+                >
+                    {this.props.children}
+                </Loader>
+            </>
+        );
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    loadingOff: () => {
+        dispatch({ type: LOADING_OFF });
+    },
+});
+
+export default connect(
+    undefined,
+    mapDispatchToProps
+)(SpinnerComponent);
