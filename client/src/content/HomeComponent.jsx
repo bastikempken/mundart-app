b8aca25605659b2531f9ced06653c07f6367b373
Sebastian Kempken <sebastiankempken@mailbox.org>
2019-10-09 17:23:56 +0200

    Logo on start page

--- client/src/content/HomeComponent.jsx
+++ client/src/content/HomeComponent.jsx
@@ -1,20 +1,46 @@
 import React from "react";
+import MundartLogo from "../media/vector.svg";
+import DblLogo from "../media/logo-dbl.gif";
 
 export const HomeComponent = () => (
     <>
-      <blockquote className={"blockquote text-center"}>
-        <h1>Die Sprache ist die Kleidung der Gedanken</h1>
-        <footer className={"blockquote-footer"}>Samuel Johnson</footer>
-      </blockquote>
+        <div style={{position:"relative"}}>
+            <div className="col">
+                <blockquote className={"blockquote text-center"}>
+                    <h1>Die Sprache ist die Kleidung der Gedanken</h1>
+                    <footer className={"blockquote-footer"}>Samuel Johnson</footer>
+                </blockquote>
+            </div>
+            <p>
+                Wem es schwer f&auml;llt zu sprechen, oder wer die M&ouml;glichkeit
+                verliert sich auszudr&uuml;cken, der ist in unserer heutigen
+                Gesellschaft h&auml;ufig eingeschr&auml;nkt. Unser Anliegen in der
+                logop&auml;dischen Praxis mundArt ist es, Menschen Wege und
+                M&ouml;glichkeiten aufzuweisen wieder mit ihren Mitmenschen zu
+                kommunizieren. Daf&uuml;r steht Ihnen unser Team von ausgebildeten
+                Logop&auml;den gerne unterst&uuml;tzend zur Seite.
+            </p>
+            <div style={{textAlign:"center"}}>
+                <img
+                    src={MundartLogo}
+                    width="300"
+                    href=""
+                    height=""
+                    className="d-inline-block align-top"
+                    alt="Mundart Logo"
+                />
+            </div>
+            <div style={{textAlign:"center",marginTop:"30px",marginBottom:"10px"}}>
+                <img
+                    src={DblLogo}
+                    width="200"
+                    href=""
+                    height=""
+                    className="d-inline-block align-top"
+                    alt="Dbl Logo"
+                />
+            </div>
+        </div>
 
-      <p>
-        Wem es schwer f&auml;llt zu sprechen, oder wer die M&ouml;glichkeit
-        verliert sich auszudr&uuml;cken, der ist in unserer heutigen
-        Gesellschaft h&auml;ufig eingeschr&auml;nkt. Unser Anliegen in der
-        logop&auml;dischen Praxis mundArt ist es, Menschen Wege und
-        M&ouml;glichkeiten aufzuweisen wieder mit ihren Mitmenschen zu
-        kommunizieren. Daf&uuml;r steht Ihnen unser Team von ausgebildeten
-        Logop&auml;den gerne unterst&uuml;tzend zur Seite.
-      </p>
     </>
 );
