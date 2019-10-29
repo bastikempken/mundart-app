a4ff1e227357d1d50a150deb99d296e0d1e50654
Sebastian Kempken <sebastiankempken@mail.de>
2019-10-29 22:23:56 +0100

    Docker File changes

--- build-docker.sh
+++ build-docker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-readonly CONATAINER_TAG=mundart-webapp
-docker build -t $CONATAINER_TAG .
+readonly CONATAINER_TAG=mundart-app
+readonly PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+
+docker build -t $CONATAINER_TAG:$PACKAGE_VERSION .
 
 #build tag
 tag_description='DOCKER_BUILD_'$(date +'%Y_%m_%d_%H_%M_%S')
-git tag -a $tag_description -m "'"$tag_description"'"
-git push origin $tag_description
+#git tag -a $tag_description -m "'"$tag_description"'"
+#git push origin $tag_description
 
 
