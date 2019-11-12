e2ff84f091f4d8053c93e87ba6cc73ca08a3201d
Sebastian Kempken <sebastiankempken@mail.de>
2019-11-12 20:02:24 +0100

    temp

--- build-docker.sh
+++ build-docker.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+set -e
+
 readonly CONATAINER_TAG=mundart-app
 readonly REMOTE_REPO=sebastiankem/$CONATAINER_TAG
-readonly PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+
+npm version major
+
+readonly VERSION=$(node -p -e "require('./package.json').version")
 #--
-docker build -t $CONATAINER_TAG:$PACKAGE_VERSION .
+docker build -t $CONATAINER_TAG:$VERSION .
 #--
+readonly $CONATAINER_ID=$(docker images --filter=reference=$CONATAINER_TAG:$VERSION --format "{{.ID}}")
+
+docker tag $CONATAINER_ID $REMOTE_REPO:$VERSION
+
+docker push $REMOTE_REPO
+
+
 #build tag
 tag_description='DOCKER_BUILD_'$(date +'%Y_%m_%d_%H_%M_%S')
 #git tag -a $tag_description -m "'"$tag_description"'"
