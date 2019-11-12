8346b291f204df1c1fb71a4279323b9512e735fe
Sebastian Kempken <sebastiankempken@mail.de>
2019-11-12 20:59:05 +0100

    Finish docker build script, reset version

--- build-docker.sh
+++ build-docker.sh
@@ -15,9 +15,3 @@ readonly CONATAINER_ID=$(docker images --filter=reference=$CONATAINER_TAG:$VERSI
 docker tag $CONATAINER_ID $REMOTE_REPO:$VERSION
 
 docker push $REMOTE_REPO
-
-
-#build tag
-tag_description='DOCKER_BUILD_'$(date +'%Y_%m_%d_%H_%M_%S')
-#git tag -a $tag_description -m "'"$tag_description"'"
-#git push origin $tag_description
