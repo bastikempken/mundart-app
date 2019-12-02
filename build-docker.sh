bd27a439ff9d9288028deb94a2f865d364258ef0
Sebastian Kempken <sebastiankempken@mail.de>
2019-12-02 21:31:59 +0100

    Organize build-docker script

--- build-docker.sh
+++ build-docker.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 set -e
-
+#------------------------
+# variables
+#------------------------
 readonly CONATAINER_TAG=mundart-app
 readonly REMOTE_REPO=sebastiankem/$CONATAINER_TAG
 
-# Build Client
+#------------------------
+# install & build client
+#------------------------
 npm run client-install
 npm run client-build
 
+#------------------------
+# version bump
+#------------------------
 npm version major
-
 readonly VERSION=$(node -p -e "require('./package.json').version")
-#--
+
+#------------------------
+# build,tag,push docker
+#------------------------
 docker build -t $CONATAINER_TAG:$VERSION .
-#--
 readonly CONATAINER_ID=$(docker images --filter=reference=$CONATAINER_TAG:$VERSION --format "{{.ID}}")
-
 docker tag $CONATAINER_ID $REMOTE_REPO:$VERSION
-
 docker push $REMOTE_REPO
 
-tag_description='DOCKER_BUILD_'$(date +'%Y_%m_%d_%H_%M_%S')
-git tag -a $tag_description -m "'"$tag_description"'"
-git push origin $tag_description
+#------------------------
+# push git
+#------------------------
+git push origin $VERSION
