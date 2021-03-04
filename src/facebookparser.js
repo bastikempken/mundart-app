c5228c4ee6fe3010934896ca6a4220b2812b103f
Sebastian Kempken <sebastian@kempken.io>
2021-03-04 16:18:25 +0100

    Init nextjs

--- src/facebookparser.js
+++
@@ -1,9 +0,0 @@
-module.exports.parseFbPosts = function(fbPosts) {
-  const posts = fbPosts.data.map(p => convert(p));
-  return JSON.stringify({ posts });
-};
-
-const convert = rawJsonEntry => ({
-  id: rawJsonEntry.id.split("_")[1],
-  timestamp: new Date(rawJsonEntry.created_time)
-});
